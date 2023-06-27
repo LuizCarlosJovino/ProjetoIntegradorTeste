6d868d9e6a439db20b23863db25f0621e24ac24a
AironLeonardo <aironprofissional@gmail.com>
2023-06-26 21:12:42 -0300

    Testando

--- ProjetoIntegrador/Program.cs
+++ ProjetoIntegrador/Program.cs
@@ -4,7 +4,7 @@ using ProjetoIntegrador;
 
 Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
 Console.WriteLine("     Seja Bem Vindo Ao Trampou     ");
-Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");//fazendo merda
+Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");//Pedro Bisneto
 Console.WriteLine("                                   ");
 
 var menu = new Menu();
